a094a755c97417e870085d4b7cd7a2d21a25e235
Fynn Muermanns <fynn.m@live.de>
2023-04-27 20:49:46 +0200

    add copyright msg

--- log.h
+++ log.h
@@ -1,3 +1,25 @@
+/*
+ Copyright (c) 2023 Fynn Mürmanns
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 #ifndef LOG_H_
 #define LOG_H_
 
@@ -390,7 +412,8 @@ class LoggerImpl {
  * set_log_format methods; this class contains methods that
  * support the logging of all primitive and most std library types;
  * if you wish to log your own custom types, please extend the
- * "LoggerImpl" class and add your own "void log(YourCustomType t)"
+ * "LoggerImpl" class and add your own
+ * "void log(std::ostream &stream, YourCustomType t, LogFormat fmt)"
  * definitions that specify how your custom types should be logged
  * and provide your extension of the LoggerImpl class as the template
  * class for the Logger class;
